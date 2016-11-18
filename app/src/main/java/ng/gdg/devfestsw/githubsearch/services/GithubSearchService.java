8eefb0ab5e675859f04c9a3d08578b60d6934c23
Faiçal Tchirou <ftchirou@gmail.com>
2016-11-18 14:28:52 +0100

    Remove unnecessary comments

--- app/src/main/java/ng/gdg/devfestsw/githubsearch/services/GithubSearchService.java
+++ app/src/main/java/ng/gdg/devfestsw/githubsearch/services/GithubSearchService.java
@@ -15,31 +15,14 @@ public interface GithubSearchService {
 
     interface Callback {
 
-        /**
-         * Called when new repositories have been fetched from the service.
-         */
         void onSuccess(List<GithubRepository> repositories);
 
-        /**
-         * Called when an error was returned from the service.
-         */
         void onError(GithubError error);
 
-        /**
-         * We have a limit of 10 requests per minutes. So, this will be
-         * called when we exceed that limit.
-         */
         void onLimitExceeded();
 
-        /**
-         * Called when the user cancels a request.
-         */
         void onCancelled();
 
-        /**
-         * Called when there is an error like a network failure, a parsing error
-         * or something else.
-         */
         void onFailure(Throwable t);
     }
 }
